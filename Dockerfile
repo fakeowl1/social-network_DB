9f0d3bf0ab1e90ed5acacb78736b4fb7b3e2210a
fakeowl1 <ukolov.roman@lll.kpi.ua>
2025-12-16 19:17:54 +0200

    fix: problems with prisma connection

--- Dockerfile
+++ Dockerfile
@@ -2,11 +2,13 @@ FROM node:25.2
 
 WORKDIR /app
 
+RUN apt install openssl
+
 COPY . . 
 RUN npm install
 
 RUN npx prisma generate
 
-EXPOSE 3000
+EXPOSE 8080
 
 CMD ["npm", "start"]
--- app.js
+++ app.js
@@ -10,7 +10,7 @@ fastify.register(usersRoutes, { prefix: 'api/v1/users' });
 
 const start = async () => {
   try {
-    await fastify.listen({ port: 3000 });
+    await fastify.listen({ port: 8080, host: '0.0.0.0' });
   } catch (err) {
     fastify.log.error(err);
     process.exit(1);
