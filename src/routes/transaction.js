f0cd980a78176aa5141038579dc920a395ba3a5f
KaterynaHl <glebakata50@gmail.com>
2025-12-18 21:33:42 +0200

    feat: add income transaction endpoint

---
+++ src/routes/transaction.js
@@ -0,0 +1,20 @@
+import { createTransaction } from '../services/transaction_service.js';
+import { getUserIdFromToken } from '../services/token_service.js';
+
+export const transactionsRoutes = async (fastify, options) => {
+
+// INCOME
+fastify.post('/income', async (req, reply) => {
+    const token = req.headers.authorization;
+    const { amount } = req.body;
+    const userId = await getUserIdFromToken(token);
+    
+    const transaction = await createTransaction({
+      userId,
+      type: 'income',
+      amount
+    });
+
+    return reply.code(201).send(transaction);
+  });
+}
